190bd263e8d0da81590595c8b59b3974cef454b5
rockleon <ameyphantom07@gmail.com>
2021-05-22 19:03:39 +0530

    set google analytics

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 import router from "./router";
 import "./plugins/axios";
 import vuetify from "./plugins/vuetify";
 import "@mdi/font/css/materialdesignicons.css";
+import VueGtag from "vue-gtag";
 
+Vue.use(
+  VueGtag,
+  {
+    config: { id: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID },
+  },
+  router
+);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   vuetify,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
